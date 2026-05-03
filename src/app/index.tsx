3b42585ec6e56df6c2ea7cd3c7f700f2d19b5a59
salmanshaikh18 <salmanshaikh18786@gmail.com>
2026-05-03 18:12:18 +0530

    chore: "Assets added

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View, StyleSheet } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-        <Text className="text-3xl font-bold text-blue-500">
+        <Text className="text-3xl font-bold text-blue-500 bg-background">
           Welcome to Nativewind!
         </Text>
     </View>
